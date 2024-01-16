823ba53c3b36c58be0e1280916b32f3c2717be21
rostykdusko <duskorostuk@gmail.com>
2024-01-16 03:56:05 +0200

    3

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+class MyPetClabMain:
+    # не плутати таке присвоєння, ми через конструктор передаємо аргумент який буде присвоєно атрибуту self.name
+    def __init__(self, name:str, surname:str, age:int=None, pets:list=[], dog_name:str=None, cat_name:str=None) -> None:
+        self.name = name
+        self.surname = surname
+        # ми можемо згруповувати передані аргументи для того щоб створити потрібний атрибут
+        self.full_name = f"{name} {surname}"
+        self.age = age
+        self.pets = pets if len(pets)>0 else "заведи собі улюбленця"
+        self.pets_dog = dog_name if "Собака" in pets else None
+        self.pets_fish = "Рибка" if "Рибка" in pets else None
+        self.pets_cat = cat_name if "Кіт" in pets else None
 
+    @property
+    def full_name_property(self):
+        return f"{self.name} {self.surname}"
